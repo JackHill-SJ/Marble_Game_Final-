13365a807c9f65b4abcfab12f397e7d9a644443a
JackHill-SJ <55606044+JackHill-SJ@users.noreply.github.com>
2021-04-26 22:44:12 -0700

    Track Mat, Final 2 Levels, SFX

---
+++ Marble Game Graybox/Assets/Scripts/MarbleRollFX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarbleRollFX : MonoBehaviour
+{
+    public AudioSource playSound;
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Track" && GetComponent<Rigidbody>().velocity.x != 0)
+        {
+            playSound.Play();
+        }
+        else
+        {
+            playSound.Stop();
+        }
+    }
+}
