f580f25aaaf647f062b96fa6672913d15a29392d
JackHill-SJ <55606044+JackHill-SJ@users.noreply.github.com>
2021-05-01 01:34:38 -0700

    Menu Asset Import

---
+++ Marble Game Graybox/Assets/Scripts/MenuClick.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+public class MenuClick : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public Sprite MenuClosed;
+    public Sprite MenuOpen;
+    
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+      if (Input.GetMouseButtonDown(0))
+        {
+            MenuClosed = MenuOpen;
+        }  
+    }
+}
---
+++ Marble Game Graybox/Assets/Scripts/WebsiteButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebsiteButton : MonoBehaviour
+{
+
+    public string URL;
+    public void Open()
+    {
+        Application.OpenURL(URL);
+        Debug.Log("Check");
+    }
+}
