0b71813bacf5a9cc028e0b9cce679ce5624528a1
JackHill-SJ <55606044+JackHill-SJ@users.noreply.github.com>
2021-05-01 18:15:28 -0700

    Music Loop Added

---
+++ Marble Game Graybox/Assets/Scripts/MusicPlayLoop.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayLoop : MonoBehaviour
+{
+    private static MusicPlayLoop instance = null;
+    public static MusicPlayLoop Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
